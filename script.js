5920b77ea7f821478eb9346d4c3af56fd0b801fb
MauikPanchal <maulik.j.panchal@hotmail.com>
2014-08-24 23:59:43 +0530

    Script File

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+ * Starting required code with below line
+ */
+
+(function($) {
+	/*
+	 * Your code between for example
+	 */
+	jQuery(document).ready(function($) {
+		/*
+		 * Your code when the Document is ready
+		 */
+	});
+
+}(jQuery));
+
+/*
+ *  Ending Required code with above line line
+ */
